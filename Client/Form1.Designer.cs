8114b6346c15421573bd39d57abfca692b0c3b8a
snakewriter <49989315+snakewriter@users.noreply.github.com>
2021-11-13 20:04:26 +0500

    -test data autoload

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Client
+{
+    partial class HarmaSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRawData = new System.Windows.Forms.DataGridView();
+            this.bLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRawData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRawData
+            // 
+            this.dgvRawData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvRawData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRawData.Location = new System.Drawing.Point(12, 55);
+            this.dgvRawData.Name = "dgvRawData";
+            this.dgvRawData.Size = new System.Drawing.Size(670, 383);
+            this.dgvRawData.TabIndex = 0;
+            // 
+            // bLoad
+            // 
+            this.bLoad.Location = new System.Drawing.Point(560, 12);
+            this.bLoad.Name = "bLoad";
+            this.bLoad.Size = new System.Drawing.Size(122, 23);
+            this.bLoad.TabIndex = 1;
+            this.bLoad.Text = "Load test data";
+            this.bLoad.UseVisualStyleBackColor = true;
+            this.bLoad.Click += new System.EventHandler(this.On_bLoadClick);
+            // 
+            // HarmaSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.Controls.Add(this.bLoad);
+            this.Controls.Add(this.dgvRawData);
+            this.Name = "HarmaSearch";
+            this.Text = "Список предприятий";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRawData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRawData;
+        private System.Windows.Forms.Button bLoad;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class HarmaSearch : Form
+    {
+        public HarmaSearch()
+        {
+            InitializeComponent();
+        }
+               
+        private void On_bLoadClick(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "*.txt|*.csv";
+            dialog.Title = "Choose the file";
+
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            try
+            {
+                var path = dialog.FileName;
+                using (var fs = File.OpenText(path))
+                {
+                    ReadStream(fs);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data format invalid or corrupted");
+            }
+        }
+
+        private void ReadStream(StreamReader sr)
+        {
+            InitColumns(sr.ReadLine());
+            for (int i = 0; i < 100; i++)
+            {
+                if (sr.EndOfStream) break;
+                AddEnterprise(sr.ReadLine());
+            }
+        }
+
+        private void InitColumns(string headers)
+        {
+            var headerParts = headers.Split(',');
+            for (int i = 0; i < 100; i++)
+            {
+                var header = headerParts[i];
+                dgvRawData.Columns.Add(header, header);
+            }
+        }
+
+        private void AddEnterprise(string dataLine)
+        {
+            var columns = dataLine.Split(',');
+            dgvRawData.Rows.Add(columns);
+        }
+    }
+}
